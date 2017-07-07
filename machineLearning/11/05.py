3bff02cb262351e37244e7376091f6107fec82f2
buko106 <buko1062000@yahoo.co.jp>
2017-07-07 22:37:04 +0900

    ID 05 submission

--- machineLearning/11/05.py
+++ machineLearning/11/05.py
@@ -9,24 +9,25 @@ Lambda = np.linalg.inv(Sigma)
 eps = 1e-4
 num = 1000
 ans = np.random.multivariate_normal(mu,Sigma,num)
+plt.subplot(211)
+plt.title("target")
 plt.axis("equal")
 plt.scatter(ans[:,0],ans[:,1])
 xlim = plt.xlim()
 ylim = plt.ylim()
-plt.pause(1.)
 
 while True:
     old_m = m.copy()
     m[0] = mu[0] - (m[1]-mu[1])*Lambda[0,1]/Lambda[0,0]
     m[1] = mu[1] - (m[0]-mu[0])*Lambda[0,1]/Lambda[1,1]
-    print(m)
     x = np.random.normal(m[0],1/Lambda[0,0],num)
     y = np.random.normal(m[1],1/Lambda[1,1],num)
-    plt.clf()
-    plt.axis("equal")
-    plt.xlim(xlim)
-    plt.ylim(ylim)
-    plt.scatter(x,y)
-    plt.pause(0.001)
     if np.linalg.norm(m-old_m) < eps:
+        plt.subplot(212)
+        plt.title("VB")
+        plt.axis("equal")
+        plt.xlim(xlim)
+        plt.ylim(ylim)
+        plt.scatter(x,y)
+        plt.savefig("result05.eps")
         break
